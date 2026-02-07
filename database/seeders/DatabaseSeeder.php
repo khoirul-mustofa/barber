1dece61283fdfc8f7167745630863b33d85f0cd7
khoirul-mustofa <khoirulmustofa102@gmail.com>
2026-02-07 14:12:49 +0700

    dev: update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             ServiceCategoriesSeeder::class,
             SettingSeeder::class,
             SpecialPackageSeeder::class,
-
+            PaymentMethodSeeder::class,
         ]);
     }
 }
