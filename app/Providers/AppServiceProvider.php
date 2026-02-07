95e95838b7bb258a056ead9cd49ef5bcdde4d122
khoirul-mustofa <khoirulmustofa102@gmail.com>
2026-02-07 18:29:01 +0700

    fix: Mixed content: load all resources via HTTPS to improve the security of your site

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use App\Models\Booking;
 use App\Observers\BookingObserver;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,13 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if (! function_exists('setting')) {
-        //     Log::warning('Helper function "setting" is not defined. Please check if app/helpers.php is loaded.');
-        // }
         Booking::observe(BookingObserver::class);
-
-        // if (app()->environment('production')) {
-        //     URL::forceScheme('https');
-        // }
     }
 }
