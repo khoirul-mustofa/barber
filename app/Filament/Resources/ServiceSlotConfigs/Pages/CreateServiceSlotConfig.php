5a4b22341c635ebb805f7f2d70a7b86256dfc4a6
khoirul-mustofa <khoirulmustofa102@gmail.com>
2026-03-17 15:27:26 +0700

    fix: redirect from form page to index

--- app/Filament/Resources/ServiceSlotConfigs/Pages/CreateServiceSlotConfig.php
+++ app/Filament/Resources/ServiceSlotConfigs/Pages/CreateServiceSlotConfig.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateServiceSlotConfig extends CreateRecord
 {
     protected static string $resource = ServiceSlotConfigResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/ServiceSlotConfigs/Pages/EditServiceSlotConfig.php
+++ app/Filament/Resources/ServiceSlotConfigs/Pages/EditServiceSlotConfig.php
@@ -16,4 +16,9 @@ class EditServiceSlotConfig extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
